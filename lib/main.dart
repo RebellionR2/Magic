bfc906ec03e11f89d9eac8f9bed2cd96718cecf9
RebellionR2 <shubhamdubey240620@gmail.com>
2024-10-06 17:58:28 +0530

    Intial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          'Ask me Anything', style: TextStyle(color:Colors.white,fontWeight: FontWeight.bold ),
+      ),
+    
+    ),
+     body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+    
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int  value=1;
+  @override
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(onPressed: (){
+         setState(() {
+           value=Random().nextInt(5)+1;
+         });
+         print('Number of $value');
+      }, child: Image.asset('images/ball$value.png')),
+    );
+  }
+}
